7f28f8ef94c3bfc0a64472c224608291bfb60176
alimahsim <alimahsim@gmail.com>
2023-01-12 16:38:39 +0500

    create variables in JS and add .gitignore

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+// This is the external coding style of JS
+
+alert("let's get started (This is in External JS)");
+console.log("Hello world!");
+
+// Creating variables 
+
+let name = "Alibek";
+let surname = "Nematov";
+let age = 36;
+let position = "translator";
+let futurePosition = "Web Developer";
+
+console.log (name); //maybe it's the key word of the js 
+console.log (surname);
+console.log (age);
+console.log (position);
+console.log (futurePosition);
+
+const id = "AA0087CA";
+console.log(id);
+
+var num1 = 5;
+var num2 = 10;
+
+console.log (num1 != num2);
+console.log (num1 == num2);
+
+
+
